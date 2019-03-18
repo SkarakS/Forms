4db01501d49ad808b2cf847717f7da845873bd6c
SkarakS <gramolin.dima@yandex.ru>
2019-03-18 19:14:23 +0500

    Form 0.01

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+function disp(form) {
+    if (form.style.display == "none") {
+        form.style.display = "block";
+    } 
+
+    else {
+        form.style.display = "none";
+    }
+    anime({
+        targets: '.name1',
+        translateY: 1250
+    });
+    if (form.style.display == "none") {
+        anime({
+            targets: '.name1',
+            translateY: 0
+        });
+    } 
+}
+    function FormUser()
+{
+        location.href = 'FormUser.html'
+    }
+
